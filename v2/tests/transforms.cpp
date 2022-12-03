2d721a0c73d3bf34e853437663f3b76f34fb70d6
oliverw10 <oliver@warrickfamily.com>
2022-12-03 12:59:52 +1100

    restoed old version

--- v2/tests/transforms.cpp
+++
@@ -1,9 +0,0 @@
-#include <Eigen/Core>
-#include <iostream>
-
-int main(){
-    Eigen::Matrix4d test_mat = Eigen::Matrix4d::Identity();
-    Eigen::Vector4d test_vec(0, 0.5, 5, 1);
-    Eigen::Matrix4d output = test_mat * test_vec;
-    std::cout << test_vec;
-}
